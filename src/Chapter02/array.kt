1f3ca980730fbaaebd2e2f25ad321528092554b4
DaeunJeong <alal8272@gmail.com>
2018-08-01 09:47:13 +0900

    add chapter02

---
+++ src/Chapter02/array.kt
@@ -0,0 +1,12 @@
+package Chapter02
+
+fun main(args: Array<String>) {
+    //배열 선언하기
+
+    //원시 타입 배열 선언
+    val colors1: IntArray = intArrayOf(1, 2, 3, 4)
+
+    //래퍼 타입 배열 선언
+    val colors2: Array<Int> = arrayOf(1, 2, 3, 4)
+    //arrayOf는 입력받은 인자로 구성된 배열을 생성하는 함수
+}
